412a9f12ee4045553751060c84c71f8d49f22563
Ivorysanna <violetta.pyralov@web.de>
2022-05-08 18:07:56 +0200

    getAvailableBikes added
    Bikes from Database without pictures

--- bike.js
+++ bike.js
@@ -1,52 +1,74 @@
 const showAvailableCollections = async () => {
-    const availableCollectionsJson = await getAvailableCollections();
-    if (availableCollectionsJson == null) {
+    const availableBikes = await getAvailableBikes();
+    if (availableBikes == null) {
         return;
     }
-
-    const getAvailableCollections = async () => {
-        try {
-            const response = await fetch("http://localhost:3000/getAllBikes", {
-                method: "GET",
-            });
-            const myJson = await response.json();
-            return myJson;
-        } catch (error) {
-            alert("error: " + error);
-            return;
-        }
-    };
 
     const collectionsContainer = document.getElementById("bicycles-container");
 
     /* Create Collections Container Structure */
-    for (let i = 0; i < availableCollectionsJson.length; i++) {
-        // Collection Div
-        const collection = document.createElement("div");
-        collection.setAttribute("class", "collection");
+    availableBikes.forEach((eachBike) => {
+        let newHtmlString = `
+        <div class="col">
+            <div class="card">
+                <img src="http://placekitten.com/400/200" class="card-img-top" alt="Katze :)">
+                <div class="card-body">
+                    <h5 class="card-title">${eachBike.Marke}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        `;
 
-        // Collection Name
-        const collectionName = document.createElement("h3");
-        collectionName.innerHTML = availableCollectionsJson[i].Marke;
-        collection.appendChild(collectionName);
+        collectionsContainer.insertAdjacentHTML("beforeend", newHtmlString);
+    });
+};
+async function getAllBikes() {
+    try {
+        const response = await fetch("http://localhost:3000/getAllBikes", {
+            method: "GET",
+        });
+        const myJson = await response.json();
+        console.log(myJson);
+        return myJson;
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
 
-        // Collection Systems
-        const systems = document.createElement("h4");
-        systems.innerHTML = "Collection Systems:";
-        collection.appendChild(systems);
+async function getAvailableBikes() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        async function getAllBikes() {
-            try {
-                const response = await fetch("http://localhost:3000/getAllBikes", {
-                    method: "GET",
-                });
-                const myJson = await response.json();
-                console.log(myJson);
-                return myJson;
-            } catch (error) {
-                console.log(error);
-                return;
-            }
-        }
+    var raw = JSON.stringify({
+        status: ["0"],
+    });
+
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    return fetch("http://localhost:3000/getBikesByFilterOptions", requestOptions)
+        .then((response) => response.json())
+        .catch((error) => console.log("error", error));
+}
+
+const getAvailableCollections = async () => {
+    try {
+        const response = await fetch("http://localhost:3000/getAllBikes", {
+            method: "GET",
+        });
+        const myJson = await response.json();
+        return myJson;
+    } catch (error) {
+        alert("error: " + error);
+        return;
     }
 };
+
+showAvailableCollections();
