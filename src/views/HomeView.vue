15e365d6acacedc7bc2cf7cc83ffaf4caf1ee900
meyerjas <meyer.jasmin1099@gmail.com>
2022-06-23 18:16:32 +0200

    button Home und CSS Bike cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -56,10 +56,8 @@
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4 whiteText">Easy - Safe - Together</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button onclick='window.location.href = "http\:\/\/127.0.0.1:5500\/bicycles.html"' type="button"
-                    class="btn btn-secondary btn-lg px-4 gap-3">
-                    START NOW
-                </button>
+                <RouterLink class="button" to="/RentABike">START NOW</RouterLink>
+
             </div>
         </div>
     </div>
