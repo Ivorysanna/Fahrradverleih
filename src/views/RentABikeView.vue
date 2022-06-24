add8b1713b8bafe6db7fda90cd6c2d6cbfe6071a
Ivorysanna <violetta.pyralov@web.de>
2022-06-24 21:20:07 +0200

    createCustomer added to database

--- src/views/RentABikeView.vue
+++ src/views/RentABikeView.vue
@@ -3,7 +3,7 @@ import Bikes from "@/components/Bikes.vue";
 import { checkOutStore } from "@/stores/checkOut";
 import { sessionStore } from "@/stores/session";
 import { mapWritableState, mapActions } from "pinia";
-import type { AxiosResponse } from "axios";
+import type { AxiosError, AxiosResponse } from "axios";
 export default {
     components: { Bikes },
     data() {
@@ -72,6 +72,28 @@ export default {
                 }
             });
         },
+        createContract() {
+            this.axios({
+                method: "post",
+                url: "/createNewContract",
+                data: {
+                    name_customer: this.loggedInCustomer!.Nachname,
+                    customer_ID: this.loggedInCustomer.Kunde_ID,
+                    bike_ID: this.selectedBike.Fahrrad_ID,
+                    date_1: this.selectedDateFrom,
+                    date_2: this.selectedDateTo,
+                    payment: this.selectedPaymentOption,
+                },
+            })
+                .then((response: AxiosResponse) => {
+                    if (response.status == 200) {
+                        alert("success!");
+                    }
+                })
+                .catch((error: AxiosError) => {
+                    console.log("error", error);
+                });
+        },
     },
     computed: {
         ...mapWritableState(checkOutStore, [
@@ -89,7 +111,7 @@ export default {
     <section id="rent-bike-now">
         <h1 id="title">RENT A BIKE NOW</h1>
         <div class="rental-progress">
-            <h1 class="border-bottom headlines">Day & Time</h1>
+            <h1 class="border-bottom headlines paddingTop">Day & Time</h1>
 
             <div class="rentalForms">
                 <div>
@@ -103,10 +125,10 @@ export default {
                     <input type="datetime-local" id="meeting-time" name="meeting-time" v-model="selectedDateTo" />
                 </div>
             </div>
-            <h1 class="border-bottom headlines">Bikes</h1>
+            <h1 class="border-bottom headlines paddingTop">Bikes</h1>
             <Bikes :bikeData="bikeData"></Bikes>
 
-            <h1 class="border-bottom headlines">Personal Data</h1>
+            <h1 class="border-bottom headlines paddingTop">Personal Data</h1>
             <div class="loginForm">
                 <div class="col-lg-3">
                     <div id="logInDiv" style="display: none">
@@ -125,8 +147,15 @@ export default {
                         </form>
                     </div>
 
-                    <input class="btn btn-primary" type="button" ref="logInButton" id="logInButton" name="answer"
-                        value="Log In" @click="logInDiv" />
+                    <input
+                        class="btn btn-primary"
+                        type="button"
+                        ref="logInButton"
+                        id="logInButton"
+                        name="answer"
+                        value="Log In"
+                        @click="logInDiv"
+                    />
                 </div>
 
                 <div class="col-lg-3">
@@ -163,53 +192,100 @@ export default {
                         </form>
                     </div>
 
-                    <input class="btn btn-primary" type="button" ref="signUpButton" id="signUpButton" name="answer"
-                        value="Sign Up" @click="signUpDiv" />
+                    <input
+                        class="btn btn-primary"
+                        type="button"
+                        ref="signUpButton"
+                        id="signUpButton"
+                        name="answer"
+                        value="Sign Up"
+                        @click="signUpDiv"
+                    />
                 </div>
             </div>
 
             <!-- <p>Payment</p> -->
-            <h1 class="border-bottom headlines">Payment</h1>
+            <h1 class="border-bottom headlines paddingTop">Payment</h1>
 
             <div class="col">
                 <div class="card col text-center">
                     <div class="card-body">
                         <h6 class="card-subtitle mb-2 text-muted">Please choose your preferred payment option</h6>
                         <form>
-                            <input type="radio" id="mc" name="payment" v-model="selectedPaymentOption"
-                                value="Mastercard" />
-                            <label for="mc"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-credit-card-2-back" viewBox="0 0 16 16">
+                            <input
+                                type="radio"
+                                id="mc"
+                                name="payment"
+                                v-model="selectedPaymentOption"
+                                value="Mastercard"
+                            />
+                            <label for="mc"
+                                ><svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    class="bi bi-credit-card-2-back"
+                                    viewBox="0 0 16 16"
+                                >
                                     <path
-                                        d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
+                                        d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z"
+                                    />
                                     <path
-                                        d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
-                                </svg> Mastercard</label> <br>
+                                        d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z"
+                                    />
+                                </svg>
+                                Mastercard</label
+                            >
+                            <br />
                             <input type="radio" id="pp" name="payment" v-model="selectedPaymentOption" value="Paypal" />
-                            <label for="pp"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-paypal" viewBox="0 0 16 16">
+                            <label for="pp"
+                                ><svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    class="bi bi-paypal"
+                                    viewBox="0 0 16 16"
+                                >
                                     <path
-                                        d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.351.351 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91.379-.27.712-.603.993-1.005a4.942 4.942 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.687 2.687 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.695.695 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016c.217.124.4.27.548.438.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.873.873 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.352.352 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32.845-5.214Z" />
-                                </svg> Paypal</label> <br>
+                                        d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.351.351 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91.379-.27.712-.603.993-1.005a4.942 4.942 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.687 2.687 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.695.695 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016c.217.124.4.27.548.438.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.873.873 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.352.352 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32.845-5.214Z"
+                                    />
+                                </svg>
+                                Paypal</label
+                            >
+                            <br />
                             <input type="radio" id="cash" name="payment" v-model="selectedPaymentOption" value="Cash" />
-                            <label for="cash"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z" />
+                            <label for="cash"
+                                ><svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    class="bi bi-cash-coin"
+                                    viewBox="0 0 16 16"
+                                >
                                     <path
-                                        d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z" />
+                                        fill-rule="evenodd"
+                                        d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"
+                                    />
                                     <path
-                                        d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z" />
+                                        d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z"
+                                    />
+                                    <path
+                                        d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z"
+                                    />
                                     <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z" />
-                                </svg> Cash</label>
+                                </svg>
+                                Cash</label
+                            >
                         </form>
                     </div>
                 </div>
             </div>
 
-
             <div>
-                <h1 class="border-bottom headlines">Checkout</h1>
+                <h1 class="border-bottom headlines paddingTop">Checkout</h1>
                 <div class="container p-4">
                     <div class="row row-cols-1 row-cols-md-4 g-4">
                         <div class="col">
@@ -261,8 +337,8 @@ export default {
                 </div>
             </div>
 
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Confirm</button>
+            <div class="col-12 paddingTop">
+                <button type="submit" class="btn btn-primary centeredButton" @click="createContract">Confirm</button>
             </div>
         </div>
     </section>
@@ -315,7 +391,7 @@ export default {
 
 #meeting-time {
     border: 3px solid gray;
-    background-color: #FFF9B2;
+    background-color: #fff9b2;
     border-radius: 5%;
     margin-right: 20px;
 }
@@ -328,5 +404,14 @@ export default {
 .payment {
     display: flex;
     align-items: center;
+}
+
+.centeredButton {
+    margin: 0 auto;
+    display: block;
+}
+
+.paddingTop {
+    padding-top: 80px;
 }
 </style>
