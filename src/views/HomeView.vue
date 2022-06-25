99c611f2ee409ec6042fe0ab1e276ca269fe4a7f
Ivorysanna <violetta.pyralov@web.de>
2022-06-25 17:42:21 +0200

    Start Now Link adjusted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -56,7 +56,7 @@
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4 whiteText">Easy - Safe - Together</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <RouterLink class="button" to="/RentABike">START NOW</RouterLink>
+                <RouterLink class="button" to="/RentABike">START NOWE</RouterLink>
 
             </div>
         </div>
